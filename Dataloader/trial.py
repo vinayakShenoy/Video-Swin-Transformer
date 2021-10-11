568889d487df888511aaacd8dd1c0320003732d3
Vinayak Shenoy <vinaya96shenoy@gmail.com>
2021-10-10 23:08:41 -0400

    made the code readable

--- Dataloader/trial.py
+++ Dataloader/trial.py
@@ -6,38 +6,58 @@ from mmcv.parallel import collate, scatter
 from mmaction.models import build_recognizer
 from mmcv.runner import load_checkpoint
 
-# Get checkpoint of model parameters
-CHECKPOINT_PATH = "../models/swin_base_patch244_window877_kinetics400_22k.pth"
+import argparse
 
-# Get config file that contains model config and pipeline
-CFG_PATH = "../configs/recognition/swin/swin_base_patch244_window877_kinetics400_22k.py"
-ANN_FILE = "../data/kinetics400/kinetics400_val_list_rawframes.txt"
-DATA_PREFIX = "../data/kinetics400/rawframes_val/"
 
-dataloader = DataLoader(ann_file=ANN_FILE, data_prefix=DATA_PREFIX)
-dataloader.load_annotations()
-results = dataloader.prepare_train_frames(0)
-data = collate([results], samples_per_gpu=1)
+def run_model(model, data, device=torch.device('cuda:0')):
+    if next(model.parameters()).is_cuda:
+        # scatter to specified GPU
+        data = scatter(data, [device])[0]
 
-config = Config.fromfile(CFG_PATH)
-config.model.backbone.pretrained = None
-model = build_recognizer(config.model, test_cfg=config.get('test_cfg'))
+    with torch.no_grad():
+        scores = model(return_loss=False, **data)[0]
 
-device = 'cuda:0' # or 'cpu'
-device = torch.device(device)
-if CHECKPOINT_PATH is not None:
-    load_checkpoint(model, CHECKPOINT_PATH, map_location=device, strict=False)
+    return scores
 
-model.cfg = config
-model.to(device)
-model.eval() # for inference
-#backbone_model = model.backbone
 
-if next(model.parameters()).is_cuda:
-    # scatter to specified GPU
-    data = scatter(data, [device])[0]
+def prepare_model(config_path, checkpoint_path):
+    config = Config.fromfile(config_path)
+    config.model.backbone.pretrained = None
+    model = build_recognizer(config.model, test_cfg=config.get('test_cfg'))
 
-with torch.no_grad():
-    scores = model(return_loss=False, **data)[0]
+    device = 'cuda:0'  # or 'cpu'
+    device = torch.device(device)
 
-print(scores.shape)
+    if checkpoint_path is not None:
+        load_checkpoint(model, checkpoint_path, map_location=device, strict=False)
+
+    model.cfg = config
+    model.to(device)
+    model.eval()
+    return model
+
+def get_data(annotation_file, data_prefix, idx=0):
+    dataloader = DataLoader(ann_file=annotation_file, data_prefix=data_prefix)
+    dataloader.load_annotations()
+    results = dataloader.prepare_train_frames(idx)
+    data = collate([results], samples_per_gpu=1)
+    return data
+
+def args_parser():
+    parser = argparse.ArgumentParser(description="Arguments for train dataloader")
+    parser.add_argument("--model_checkpoint", type=str, help="Location to model (.pth) file")
+    parser.add_argument("--model_config", type=str, help="Locationto config file")
+    parser.add_argument("--annotation_file", type=str, help="Path to annotation file")
+    parser.add_argument("--data_prefix", type=str, help="Data location prefix")
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = args_parser()
+    data = get_data(args.annotation_file, args.data_prefix)
+    model = prepare_model(args.model_config, args.model_checkpoint)
+    print(run_model(model, data))
+
+if __name__ == '__main__':
+    main()
